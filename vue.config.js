df04d7820f3e00f23b6f44ba1f199a02c7007f3d
yhlk <tael002@student.kristiania.no>
2024-11-15 11:53:23 +0100

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.VUE_APP_BASE_URL || "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/population-datavisu/" : "/",
 });
