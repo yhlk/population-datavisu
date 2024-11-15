5dfc08051e2bb5e17879e2fc8f7fccefd8409013
yhlk <tael002@student.kristiania.no>
2024-11-15 11:48:26 +0100

    fixing pages

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.VUE_APP_BRANCH || "/", // Dynamically set publicPath based on environment variable
+  publicPath: process.env.VUE_APP_BASE_URL || "/",
 });
